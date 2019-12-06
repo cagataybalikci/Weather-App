d237510adb7d62460182525eb0b55ad320545623
cagataybalikci <cagataybalikci@gmail.com>
2019-12-06 17:28:26 +0300

    Skycons and DarkSky API implemented

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ window.addEventListener("load", () => {
   );
   let temperatureDegree = document.querySelector(".temperature-degree");
   let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".temperature span");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -20,13 +22,35 @@ window.addEventListener("load", () => {
         })
         .then(data => {
           console.log(data);
-          const { temperature, summary } = data.currently;
+          const { temperature, summary, icon } = data.currently;
           // Set DOM element from the API
-
           temperatureDegree.textContent = temperature;
           temperatureDescription.textContent = summary;
           locationTimezone.textContent = data.timezone;
+          // formula for F to C
+          let celcius = (temperature - 32) / 1.8;
+          // Set Icon
+          setIcons(icon, document.querySelector(".icon"));
+
+          // change temperature from Fahrenheit to Celcius
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celcius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+          });
         });
     });
+  }
+
+  // Skycons
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
